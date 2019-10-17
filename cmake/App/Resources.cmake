2c5ecaba063a5fff265c1599fe313f8fe6e007ae
one-thunder <one@thunderstorm.pro>
2019-10-17 21:39:39 +0300

    Remove copying zlibdll

--- cmake/App/Resources.cmake
+++ cmake/App/Resources.cmake
@@ -4,12 +4,6 @@
 # Use of this source code is governed by a MIT-style license that
 # can be found in the LICENSE file.
 
-# Library specific dependencies required for test app.
-ExternalProject_Get_property(zlib INSTALL_DIR)
-add_resources(Root
-	"${INSTALL_DIR}/bin/zlibd.dll"
-)
-
 # Application specific dependencies.
 add_resources(Config
 	"${PROJECT_RSRC_PATH}/${target}/config.yaml"
