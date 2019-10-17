2692743ccaeeb0d3482874fadf48b833a53bb688
Alexei Gaidulean <32410574+one-thunder@users.noreply.github.com>
2019-10-17 22:08:22 +0300

    [#71] MPIR is not exporting correctly in dependent projects (#72)
    * [#71] MPIR is not exporting correctly in dependent projects
    
    * Changed zlib to use static lib instead of dll
    
    * Formatting
    
    * Remove copying zlibdll

--- cmake/ThirdParty/zlib.cmake
+++ cmake/ThirdParty/zlib.cmake
@@ -13,8 +13,11 @@ set(LIBRARY_URL https://www.zlib.net/zlib-1.2.11.tar.gz)
 # Set library URL hash.
 set(LIBRARY_URL_HASH 33f4b3034501a3cc2e8bc8d62e70fecc)
 
-# Set library static name.
-set(LIBRARY_STATIC_NAME zlibd.lib)
+# Set debug library static name.
+set(LIBRARY_STATIC_NAME_DEBUG zlibstaticd.lib)
+
+# Set debug library static name.
+set(LIBRARY_STATIC_NAME_RELEASE zlibstatic.lib)
 
 # Enqueue to download, build & install.
 enqueue_external_dependency()
